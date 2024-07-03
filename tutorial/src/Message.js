d7b411f385c2412190e4e22bd343c2ac4bc4fd2c
Samyak Mehta <samyakmehta28@gmail.com>
2024-07-03 14:59:08 -0700

    learnt basics of JSX

---
+++ tutorial/src/Message.js
@@ -0,0 +1,10 @@
+// in HTML we use all lower, in JSX we use camelCase
+
+const Message = () => {
+  return (
+    <div>
+      <p>Welcome Samyak Mehta</p>
+    </div>
+  );
+};
+export default Message;
--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const img =
+  'https://images-na.ssl-images-amazon.com/images/I/81q6ecxcZUL._AC_UL600_SR600,400_.jpg';
+const alt = 'Reckless (The Powerless Trilogy)';
+const title = 'Reckless (The Powerless Trilogy)';
+const author = 'Lauren Roberts';
+
+const BookList = () => {
+  return (
+    <section className="bookList">
+      <Book img={img} alt={alt} title={title} author={author} />
+      <Book img={img} alt={alt} title={title} author={author} />
+      <Book img={img} alt={alt} title={title} author={author} />
+      <Book img={img} alt={alt} title={title} author={author} />
+      <Book img={img} alt={alt} title={title} author={author} />
+      <Book img={img} alt={alt} title={title} author={author} />
+    </section>
+  );
+};
+
+const Book = (props) => {
+  return (
+    <article className="book">
+      <img src={props.img} alt={props.alt} />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
+    </article>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<BookList />);
