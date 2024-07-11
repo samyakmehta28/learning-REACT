c0955a61f9a1555f56ba92e94e6464475e45310f
Samyak Mehta <samyakmehta28@gmail.com>
2024-07-10 21:26:02 -0700

    learnt conditonal rendering

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,15 @@
 import { useState } from 'react';
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  const [dummy, setDummy] = useState('dummy value');
+  const [name, setName] = useState('john doe');
+
+  return (
+    <div>
+      <h4> {dummy || name}</h4>
+
+      <h4> {dummy && name} </h4>
+    </div>
+  );
 };
 export default ShortCircuitOverview;
--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react';
 
+//instead of using if else statements inside return of JSX (which is not allowed), we can use short circuit evaluation to conditionally render elements
+
+const SomeComponent = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
 const ShortCircuitExamples = () => {
   // falsy
   const [text, setText] = useState('');
@@ -8,7 +14,24 @@ const ShortCircuitExamples = () => {
   const [user, setUser] = useState({ name: 'john' });
   const [isEditing, setIsEditing] = useState(false);
 
-  return <h2>short circuit - examples</h2>;
+  return (
+    <div>
+      {/*conditionally render elements using short circuit evaluation*/}
+      <h2>{!isEditing && 'No one editing'}</h2>
+      {isEditing && (
+        <>
+          <h1>Editing</h1>
+          <h2>{name}</h2>
+        </>
+      )}
+      {/* conditomally render components */}
+      {user && <SomeComponent name={user.name} />}
+
+      <button className="btn" onClick={() => setIsEditing(!isEditing)}>
+        {isEditing ? 'edit' : 'add'}
+      </button>
+    </div>
+  );
 };
 
 export default ShortCircuitExamples;
